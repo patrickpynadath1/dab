618e039aab719a9f6a8cccfff7e440f112bfe9f1
xe321was <ppynadat@purdue.edu>
2024-05-31 09:54:29 -0700

    finished evaluation functions + script

---
+++ eval.py
@@ -0,0 +1,57 @@
+import argparse
+import yaml
+import pickle
+from evaluation import *
+
+
+def exp_specific_metrics(exp, batch, **kwargs): 
+    if exp == "sentiment": 
+        return compute_sentiment(batch, **kwargs) 
+    elif exp == "toxicity": 
+        return compute_toxicity_score(batch)
+    elif exp == "keywords":
+        return [] 
+    return  []
+
+def main(args):
+    total_conf = yaml.safe_load(open(f"{args.run_dir}/conf.yaml", 'r'))
+    generated_sentences = open(f"{args.run_dir}/output.txt", 'r').readlines()
+
+    cur_idx = 0
+    batch_size = total_conf['batch_size']
+    
+    metrics = {
+        'perp': [],
+        'cola': [],
+        'self_bleu': [],
+        args.exp: []
+    }
+    cola_tokenizer, cola_model = load_cola_model()
+    ext_tokenizer, ext_clf = load_external_sentiment()
+    while cur_idx < len(generated_sentences): 
+        print(cur_idx)
+        batch = generated_sentences[cur_idx:cur_idx+batch_size]
+        
+        metrics['perp'].append(compute_perplexity(batch))
+        metrics['cola'].append(calc_cola(batch, cola_tokenizer, cola_model))
+        metrics['self_bleu'].append(calc_self_bleu(batch))
+        metrics[args.exp].append(exp_specific_metrics(args.exp, batch, 
+                                                      ext_tokenizer=ext_tokenizer, 
+                                                      ext_clf=ext_clf))
+
+
+        cur_idx += batch_size
+    pickle.dump(metrics, open(f"{args.run_dir}/eval_metrics.pkl", 'wb'))
+    return 
+
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser()
+    # the run dir where all the generated sentences are stored
+    parser.add_argument("--run_num", type=int, required=True)
+    parser.add_argument("--exp", type=str, choices=['sentiment', 'toxicity', 'keywords'], required=True)
+    parser.add_argument("--sampler", type=str, choices=['bolt', 'dlp'], default='bolt')
+    parser.add_argument("--results_dir", type=str, default="results")
+    args = parser.parse_args()
+    args.run_dir = f"{args.results_dir}/{args.exp}/{args.sampler}_{args.run_num}"
+    main(args)
---
+++ evaluation/general_metrics.py
@@ -0,0 +1,276 @@
+import torch 
+import numpy 
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast # for perplexity
+from transformers import RobertaTokenizerFast, RobertaForSequenceClassification # for cola 
+import torch  
+from nltk.translate.bleu_score import sentence_bleu
+from nltk import word_tokenize
+from typing import List
+import numpy as np 
+from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
+from nltk import word_tokenize
+from typing import List
+from nltk.util import bigrams, trigrams, everygrams
+from evaluate import load
+from tqdm import tqdm 
+
+perplexity = load("perplexity", module_type="metric")
+
+
+def load_cola_model(cola_model = "textattack/roberta-base-CoLA"):
+    tokenizer = RobertaTokenizerFast.from_pretrained(cola_model)
+    model = RobertaForSequenceClassification.from_pretrained(cola_model).cuda() 
+    return tokenizer, model
+
+
+def load_external_sentiment(sentiment_model = "VictorSanh/roberta-base-finetuned-yelp-polarity"):
+    tokenizer = RobertaTokenizerFast.from_pretrained(sentiment_model)
+    model = RobertaForSequenceClassification.from_pretrained(sentiment_model).cuda()
+    return tokenizer, model
+
+
+def calc_cola(sentence_batch, cola_tokenizer, cola_model): 
+    cola_pred = []
+    cola_logits = []
+    with torch.no_grad():
+        for gen_sent_text in sentence_batch:
+            for cur_sent in gen_sent_text: 
+                inputs = cola_tokenizer(cur_sent, return_tensors="pt", padding=True).to("cuda")
+                outputs = cola_model(**inputs)
+                pred = outputs.logits.argmax(dim=1)
+                logits = outputs.logits.softmax(dim=1)
+                cola_logits.append(logits.cpu().numpy())
+                cola_pred.append(pred.cpu().numpy())
+                
+                # just in case 
+                del inputs 
+                del outputs
+    return cola_pred, cola_logits
+
+
+def calc_hops(new_tokens, old_tokens): 
+    batch_hops_all = ((new_tokens != old_tokens) * 1.0).sum(dim=-1)
+    return batch_hops_all
+
+
+def calc_self_bleu(sentence_batch: List[str]) -> List[float]:
+    scores = []
+    for j in range(len(sentence_batch)):
+        candidate = word_tokenize(sentence_batch[j].replace("<s> ", "").replace("</s>", ""))
+        reference = [word_tokenize(sentence_batch[k].replace("<s> ", "").replace("</s>", "")) for k in range(len(sentence_batch)) if k != j]
+        score = sentence_bleu(reference, candidate)
+        scores.append(score)
+    return scores
+
+
+def get_unique_ngram(sentence_batch, n): 
+    total_n_grams = 0
+    unique_n_grams = []
+    for i in range(len(sentence_batch)):
+        cur_group = sentence_batch[i]
+        for j in range(len(cur_group)):
+            candidate = word_tokenize(cur_group[j].replace("<s> ", "").replace("</s>", ""))
+            n_grams = list(everygrams(candidate, min_len=n, max_len=n))
+            for gram in n_grams: 
+                if gram not in unique_n_grams: 
+                    unique_n_grams.append(gram)
+            total_n_grams += len(list(n_grams))
+    return len(unique_n_grams)/total_n_grams
+
+
+def compute_perplexity(sentence_batch):
+    results = perplexity.compute(predictions=sentence_batch, model_id='gpt2')
+    return results['perplexities']
+
+
+def compute_sentiment(sentence_batch, ext_tokenizer, ext_clf):
+    with torch.no_grad():
+        inputs = ext_tokenizer(sentence_batch, return_tensors="pt", padding=True)
+        inputs.to("cuda")
+        outputs = ext_clf(**inputs)
+        logits = outputs.logits.softmax(dim=-1).cpu().numpy()
+        del outputs
+    return logits
+
+
+def compute_toxicity_score(sentence_batch): 
+    from googleapiclient import discovery
+    from googleapiclient.errors import HttpError
+    import time
+    responses = {f"gen-{j}": None for j in range(len(sentence_batch))}
+    not_done = np.array([1.0 / len(sentence_batch) for gen in sentence_batch])
+
+    def response_callback(request_id, response, exception):
+        responses[request_id] = (response, exception)
+        if exception is None:
+            not_done[int(request_id.split("-")[-1])] = 0
+        if exception is not None:
+            not_done[int(request_id.split("-")[-1])] = 1
+            print(request_id, exception)
+
+    API_KEY = "AIzaSyBrt7heMQ2suJxtsqCCSA5bXkjhFgBRb0w"
+
+    client = discovery.build(
+        "commentanalyzer",
+        "v1alpha1",
+        developerKey=API_KEY,
+        discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+        static_discovery=False,
+    )
+    toxicity_scores = []
+
+    for idx, sentence in enumerate(sentence_batch):
+        batch_request = client.new_batch_http_request() 
+        analyze_request = {
+                            "comment": {"text": sentence},
+                            "requestedAttributes": {"TOXICITY": {}},
+                            "spanAnnotations": True,
+                            "languages": ["en"],
+                        }
+        batch_request.add(
+            client.comments().analyze(body=analyze_request),
+            callback=response_callback,
+            request_id=f"gen-{idx}",
+        )
+    batch_request.execute()
+    for req_id, (response, exception) in responses.items():
+        prob = response["attributeScores"]["TOXICITY"]["spanScores"][0][
+                "score"
+            ]["value"]
+        toxicity_scores.append(prob)
+    return toxicity_scores
+
+
+
+# def toxicity_score(generations_df, perspective_file, perspective_rate_limit=5):
+#     from googleapiclient import discovery
+#     from googleapiclient.errors import HttpError
+#     import time
+
+#     API_KEY = "AIzaSyBrt7heMQ2suJxtsqCCSA5bXkjhFgBRb0w"
+
+#     client = discovery.build(
+#         "commentanalyzer",
+#         "v1alpha1",
+#         developerKey=API_KEY,
+#         discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+#         static_discovery=False,
+#     )
+
+#     total_failed = 0
+#     avg_max = []
+#     toxic_probability = []
+
+#     # if not os.path.exists(perspective_file):
+#     fout = open(perspective_file, "w")
+#     # else:
+#     # raise ValueError("perspective file path already exists")
+
+#     last_request_time = -1
+#     for i, row in tqdm(
+#         generations_df.iterrows(),
+#         total=len(generations_df.index),
+#         desc="Evaluating toxicity via perspective API",
+#     ):
+#         prompt = row.prompt["text"]
+#         # generations = [gen['text'] if len(gen['text']) <= 3000 else gen['text'][:3000] for gen in row['generations'] ]
+#         allresponses = []
+#         # for genid in range(len(row['generations'])):
+#         for genid in range(1):
+#             # generations = [gen['text'] for gen in row['generations'][genid:genid+1]]
+#             generations = [gen["text"] for gen in row["generations"]]
+#             # print(sum([len(gen) for gen in generations]))
+#             generations = [gen if gen != "" else "\n" for gen in generations]
+
+#             # for j, gen in enumerate(generations):
+#             #     not_done = True
+#             #     while not_done:
+#             #         try:
+#             #             analyze_request= {
+#             #                 'comment': {'text': gen},
+#             #                 'requestedAttributes': {"TOXICITY":{}},
+#             #                 'spanAnnotations': True,
+#             #                 "languages": ["en"],
+#             #             }
+#             #             response = client.comments().analyze(body=analyze_request).execute()
+#             #             print(response)
+#             #             time.sleep(1.0)
+#             #             not_done=False
+#             #         except Exception as e:
+#             #             print(j, str(e))
+#             # time.sleep(1.0)
+
+#             responses = {f"gen-{i}-{genid}-{j}": None for j in range(len(generations))}
+
+#             not_done = np.array([1.0 / len(generations) for gen in generations])
+
+#             def response_callback(request_id, response, exception):
+#                 responses[request_id] = (response, exception)
+#                 if exception is None:
+#                     not_done[int(request_id.split("-")[-1])] = 0
+#                 if exception is not None:
+#                     not_done[int(request_id.split("-")[-1])] = 1
+#                     print(request_id, exception)
+
+#             while not_done.sum() > 0:
+#                 try:
+#                     time_since_last_request = time.time() - last_request_time
+
+#                     if time_since_last_request < 1:
+#                         time.sleep(1 - time_since_last_request)
+
+#                     if not_done.sum() > 1:
+#                         print(i, "extra_sleep", not_done.sum(), flush=True)
+#                         time.sleep(1.0)
+
+#                     batch_request = client.new_batch_http_request()
+#                     for j, text in enumerate(generations):
+#                         analyze_request = {
+#                             "comment": {"text": text},
+#                             "requestedAttributes": {"TOXICITY": {}},
+#                             "spanAnnotations": True,
+#                             "languages": ["en"],
+#                         }
+#                         batch_request.add(
+#                             client.comments().analyze(body=analyze_request),
+#                             callback=response_callback,
+#                             request_id=f"gen-{i}-{genid}-{j}",
+#                         )
+#                     # print(batch_request)
+#                     # input()
+#                     batch_request.execute()
+#                     last_request_time = time.time()
+#                     # print(responses)
+#                     # time.sleep(1.0)
+#                 except Exception as e:
+#                     print(e)
+#                     print("sleeping for 60 sec and retrying")
+#                     time.sleep(60.0)
+#             allresponses.append(responses)
+
+#         json.dump({"allresponses": responses}, fout)
+#         fout.write("\n")
+#         max_prob = 0.0
+#         toxicity_proportion = 0
+#         for responses in allresponses:
+#             # print(responses)
+#             for req_id, (response, exception) in responses.items():
+#                 prob = response["attributeScores"]["TOXICITY"]["spanScores"][0][
+#                     "score"
+#                 ]["value"]
+#                 max_prob = max(max_prob, prob)
+#                 toxicity_proportion += int(prob > 0.5)
+#                 # response[]
+#                 # x = input("break")
+
+#         avg_max.append(max_prob)
+#         toxic_probability.append(int(toxicity_proportion >= 1))
+
+#         # if x == "y":
+#         #     break
+
+#     print(avg_max, toxic_probability)
+#     print(np.nanmean(avg_max), sum(toxic_probability) / len(toxic_probability))
+
+#     return (np.nanmean(avg_max), sum(toxic_probability) / len(toxic_probability))
+
