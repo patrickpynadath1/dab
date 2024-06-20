cd0906a626d78b51b8594a4a0dac09cc11361fbd
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2024-06-20 14:43:35 -0400

    added the bash script for kicking off jobs

---
+++ initial_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# FILENAME:  initial_job.sh
+
+module load anaconda/2020.11-py38
+
+# should be run from the project directory 
+cd $SLURM_SUBMIT_DIR
+pip install -r requirements.txt
+
+# initial setup for transformers 
+cd transformers
+pip install -e .
+cd ..
+
+# first try 
+python main.py --exp sentiment --device cuda bolt; 
+python main.py --exp sentiment --device cuda dlp; 
