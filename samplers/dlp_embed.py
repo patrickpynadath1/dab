e9e523e357def6a8a468414b49b8aedf36f7b939
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2024-06-27 12:25:33 -0400

    finished implementing (again) top p filtering

--- samplers/dlp_embed.py
+++ samplers/dlp_embed.py
@@ -6,15 +6,11 @@ import numpy as np
 class LangevinSampler(nn.Module):
     def __init__(self, 
                  weight_val, 
-                 k_val, 
                  proposal_temp,
                  device,
-                 use_diverse_initialization=False,
-                 diverse_type='beam',
-                 num_beams=100, 
-                 num_beam_groups=100,
-                 diversity_penalty=.8, 
-                 diverse_addition_length=3, 
+                 filter_type,
+                 p_val,
+                 k_val, 
                  **kwargs):
         super().__init__()
         self.weight_val = weight_val
@@ -22,13 +18,9 @@ class LangevinSampler(nn.Module):
         self.hops = []
         self.k_val = k_val 
         self.temp = proposal_temp
-        self.diverse_type = diverse_type
         self.device = device
-        self.use_diverse_initialization=use_diverse_initialization
-        self.diverse_addition_length = diverse_addition_length
-        self.num_beams = num_beams
-        self.num_beam_groups = num_beam_groups
-        self.diversity_penalty = diversity_penalty
+        self.filter_type = filter_type
+        self.p_val = p_val
     
     def initialize_batch(self,
                          model, 
@@ -38,70 +30,23 @@ class LangevinSampler(nn.Module):
                          prompt_length,
                          inputs,
                          **kwargs):
-        if self.use_diverse_initialization: 
-            return self.diverse_initialization(model, 
-                                                sentiment,
-                                                batch_size, 
-                                                seq_length, 
-                                                prompt_length, 
-                                                inputs)
-        else: 
-            self.prompt_length = prompt_length
-            model.set_biases(batch_size=batch_size, 
-                            seq_len=seq_length,
-                            prompt_length=prompt_length, 
-                            attribute=sentiment,
-                            device=self.device)
-            initial_bias = torch.zeros(batch_size, 
-                            seq_length - prompt_length, 
-                            50257).to(self.device)
-            self.embed_map = model.get_input_embeddings()
-            return inputs, initial_bias
-    
-    def diverse_initialization(self,
-                         model, 
-                         sentiment,
-                         batch_size,
-                         seq_length,
-                         prompt_length,
-                         inputs,
-                         **kwargs): 
-        self.prompt_length = prompt_length + self.diverse_addition_length
-        # inputs.input_ids = inputs.input_ids[0, :]
-        # inputs.attention_mask = torch.ones_like(inputs.input_ids).to(self.device)
-        new_inputs = model.generate(
-            input_ids=inputs.input_ids[0, :].unsqueeze(0),
-            num_return_sequences=self.num_beam_groups,
-            top_k=batch_size,
-            num_beams=self.num_beams,
-            num_beam_groups=self.num_beam_groups,
-            bad_words_ids=[[198], [628]],
-            max_new_tokens=self.diverse_addition_length,
-            diversity_penalty=self.diversity_penalty,
-        )
-        new_inputs = new_inputs.to(self.device)
-        # running the discriminator to get the best sentences 
-        # disc_embeds = model.discriminator.get_input_embeddings()(new_inputs)
-        disc_logits = model.discriminator(new_inputs).logits
-        loss = disc_logits[:, 1] - disc_logits[:, 0]
-        best_idx = torch.topk(loss, batch_size).indices
-        new_inputs = new_inputs[best_idx]
+        self.prompt_length = prompt_length
         model.set_biases(batch_size=batch_size, 
-                         seq_len=seq_length,
-                         prompt_length=self.prompt_length,
-                         attribute=sentiment,
-                         device=self.device)
+                        seq_len=seq_length,
+                        prompt_length=prompt_length, 
+                        attribute=sentiment,
+                        device=self.device)
         initial_bias = torch.zeros(batch_size, 
-                           seq_length - self.prompt_length, 
-                           50257).to(self.device)
+                        seq_length - prompt_length, 
+                        50257).to(self.device)
         self.embed_map = model.get_input_embeddings()
-        return new_inputs, initial_bias
-
-    def compute_p_lm(self, 
-                     cur_bias, 
-                     energy_fn): 
+        return inputs, initial_bias
         
-        loss, output_ids, onehot, logits, senti_losses = energy_fn(cur_bias)
+
+    def get_top_k_logits(self, 
+                         loss, 
+                         onehot, 
+                         logits): 
         gx = torch.autograd.grad(loss, onehot, allow_unused=True)
         gx = gx[0].detach()[:, self.prompt_length:, :]
         logits = logits[:, self.prompt_length:, :]
@@ -109,11 +54,57 @@ class LangevinSampler(nn.Module):
         gx_topk = gx[torch.arange(gx.size(0))[:, None, None], 
                      torch.arange(gx.size(1))[None, :, None], 
                      topk_ids]
-        # gx_topk = torch.gather(gx, -1, topk_ids)
         token_dist = torch.ones_like(gx_topk).to(self.device) 
         token_dist[:, :, 0] = 0
-        logits = gx_topk * token_dist
-        proposal_dist = torch.distributions.Categorical(logits = -1 * logits / self.temp)
+        dist_logits = gx_topk * token_dist
+        return dist_logits, topk_ids
+    
+
+    # first need to sort the logits 
+    # then need to do torch cum sum in order to figure out where the p cut off is 
+    # then need to take the top p logits, where the dimension is the largest out of all sequences 
+    # 
+    def get_top_p_logits(self, 
+                         loss,
+                         onehot,
+                         logits):
+        gx = torch.autograd.grad(loss, onehot, allow_unused=True)
+        gx = gx[0].detach()[:, self.prompt_length:, :]
+        logits = logits[:, self.prompt_length:, :].softmax(dim=-1)
+        sorted_logits, sorted_indices = torch.sort(logits, dim=-1, descending=True)
+        cumsum_logits = torch.cumsum(sorted_logits, dim=-1)
+        cumsum_cutoff_idx = ((cumsum_logits > self.p_val) * 1.0).argmax(dim=-1)
+        cumsum_cutoff_idx = torch.where(cumsum_cutoff_idx == 0, cumsum_cutoff_idx, 1)
+        max_cutoff_idx = cumsum_cutoff_idx.max()
+
+
+        # calculating the logits by the standard dlp method 
+        topk_ids = sorted_indices[:, :max_cutoff_idx]
+        gx_topk = gx[torch.arange(gx.size(0))[:, None, None], 
+                     torch.arange(gx.size(1))[None, :, None], 
+                     topk_ids]
+        token_dist = torch.ones_like(gx_topk).to(self.device) 
+        token_dist[:, :, 0] = 0
+        dist_logits_premask = gx_topk * token_dist
+
+        # now need to mask out the logits that are not in the top p
+        tmp_mask = dist_logits_premask / ((cumsum_logits < self.p_val)*1.0)[:, :, :max_cutoff_idx]
+        # this gets multipled by -1 later, so need to make it pos inf for right now 
+        masked_logits = torch.where(tmp_mask == float('-inf'), torch.tensor(float('inf'), dtype=tmp_mask.dtype), tmp_mask)
+        # making sure at least one index is a scalar value, or else the torch.distributions.Categorical will throw an error
+        masked_logits[:, :, 0] = dist_logits_premask[:, :, 0]
+        return masked_logits, topk_ids
+
+    def compute_p_lm(self, 
+                     cur_bias, 
+                     energy_fn): 
+
+        loss, output_ids, onehot, logits, senti_losses = energy_fn(cur_bias)
+        if self.filter_type == "topk":
+            dist_logits, topk_ids = self.get_top_k_logits(loss, onehot, logits)
+        elif self.filter_type == "topp":
+            dist_logits, topk_ids = self.get_top_p_logits(loss, onehot, logits)
+        proposal_dist = torch.distributions.Categorical(logits = -1 * dist_logits / self.temp)
         sampled_dist_ids = proposal_dist.sample()
         actual_ids = topk_ids[torch.arange(topk_ids.size(0))[:, None],
                               torch.arange(topk_ids.size(1))[None, :],
