77bdeece21eff5ceb9d3a4b8c0419679fd99c23e
xe321was <ppynadat@purdue.edu>
2024-09-19 06:37:00 -0700

    cleaning up the repo

---
+++ api_eval_prompts.py
@@ -0,0 +1,22 @@
+from eval import compute_perspective_scores, clean_for_eval
+
+
+
+
+def main(): 
+    detoxic_prompts = clean_for_eval(
+        open(f"prompts/detoxic_prompts_1k.txt", "r").readlines()
+    )
+    print(f"num of sentences {len(detoxic_prompts)}")
+    cur_batch = detoxic_prompts
+    compute_perspective_scores(
+        cur_batch,
+        '.',
+        start_idx=0,
+        rate_limit=60,
+    )
+
+
+
+if __name__ == "__main__":
+    main()
